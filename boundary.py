518287c4ea34436342657979c3bd92a480ed6392
Abishek <abishek.t@egovernments.org>
2025-09-26 18:49:52 +0530

    changes for boundat_data for demo

--- boundary.py
+++ boundary.py
@@ -1,127 +1,219 @@
-boundary_data = [
+kebbi_state_data = [
     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kebbi state",
-              "district": "Kebbi",
-              "locality": "Gawasu damana",
-              "village": "Ke akwara health post",
-              "administrativeProvince": "Birnin kebbi"
-            },
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kebbi state"
+        },
         "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE",
-              "district": "MICROPLAN_MO_17_01_KEBBI",
-              "locality": "MICROPLAN_MO_17_01_01_03_GAWASU_DAMANA",
-              "village": "MICROPLAN_MO_17_01_01_03_01_KE_AKWARA_HEALTH_POST",
-              "administrativeProvince": "MICROPLAN_MO_17_01_01_BIRNIN_KEBBI"
-            }
-    },
-     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kebbi state",
-              "district": "Kebbi",
-             
-            },
-        "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE",
-              "district": "MICROPLAN_MO_17_01_KEBBI"
-            }
-    },
-     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kebbi state",
-              
-             
-            },
-        "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE"
-            }
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_17_KEBBI_STATE"
+        }
     },
     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Grand Gedeh",
-              "district": "Tchien",
-              "locality": "Toffoi Town",
-              "village": "Satya Test",
-              "administrativeProvince": "Toffoi Clinic"
-            },
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kebbi state",
+            "district": "Kebbi"
+        },
         "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_13_GRAND_GEDEH",
-              "district": "MICROPLAN_MO_13_05_TCHIEN",
-              "locality": "MICROPLAN_MO_13_05_04_01_TOFFOI_TOWN",
-              "village": "MICROPLAN_MO_13_05_04_01_12_SATYA_TEST",
-              "administrativeProvince": "MICROPLAN_MO_13_05_04_TOFFOI_CLINIC"
-            },
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_17_KEBBI_STATE",
+            "district": "MICROPLAN_MO_17_01_KEBBI"
+        }
     },
     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Grand Gedeh",
-              "district": "Tchien"
-            },
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kebbi state",
+            "district": "Kebbi",
+            "administrativeProvince": "Birnin kebbi"
+        },
         "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_13_GRAND_GEDEH",
-              "district": "MICROPLAN_MO_13_05_TCHIEN"
-            },
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_17_KEBBI_STATE",
+            "district": "MICROPLAN_MO_17_01_KEBBI",
+            "administrativeProvince": "MICROPLAN_MO_17_01_01_BIRNIN_KEBBI"
+        }
     },
     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Grand Gedeh"
-            },
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kebbi state",
+            "district": "Kebbi",
+            "locality": "Gawasu damana",
+            "administrativeProvince": "Birnin kebbi"
+        },
         "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_13_GRAND_GEDEH"
-            },
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_17_KEBBI_STATE",
+            "district": "MICROPLAN_MO_17_01_KEBBI",
+            "locality": "MICROPLAN_MO_17_01_01_03_GAWASU_DAMANA",
+            "administrativeProvince": "MICROPLAN_MO_17_01_01_BIRNIN_KEBBI"
+        }
     },
-     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kano state",
-              "district": "Kano",
-              "locality": "Balare l",
-              "village": "Maigana hp",
-              "administrativeProvince": "Ajingi"
-            },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kebbi state",
+            "district": "Kebbi",
+            "locality": "Gawasu damana",
+            "village": "Ke akwara health post",
+            "administrativeProvince": "Birnin kebbi"
+        },
         "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE",
-              "district": "MICROPLAN_MO_17_01_KEBBI",
-              "locality": "MICROPLAN_MO_17_01_01_01_AMBURSA",
-              "village": "MICROPLAN_MO_17_01_01_01_01_KE_AMBURSA_PRIMARY_HEALTH_CENTRE",
-              "administrativeProvince": "MICROPLAN_MO_17_01_01_BIRNIN_KEBBI"
-            },
-    },
-     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kano state",
-              "district": "Kano"
-            },
-        "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE",
-              "district": "MICROPLAN_MO_17_01_KEBBI"
-            }
-    },
-     {
-        "hierarchy":{
-              "country": "Nigeria",
-              "province": "Kano state"
-            },
-        "codes": {
-              "country": "MICROPLAN_MO",
-              "province": "MICROPLAN_MO_17_KEBBI_STATE"
-            }
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_17_KEBBI_STATE",
+            "district": "MICROPLAN_MO_17_01_KEBBI",
+            "locality": "MICROPLAN_MO_17_01_01_03_GAWASU_DAMANA",
+            "village": "MICROPLAN_MO_17_01_01_03_01_KE_AKWARA_HEALTH_POST",
+            "administrativeProvince": "MICROPLAN_MO_17_01_01_BIRNIN_KEBBI"
+        }
     }
-    
+]
 
-    ]
+
+grand_gedeh_data = [
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Grand Gedeh"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_13_GRAND_GEDEH"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Grand Gedeh",
+            "district": "Tchien"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_13_GRAND_GEDEH",
+            "district": "MICROPLAN_MO_13_05_TCHIEN"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Grand Gedeh",
+            "district": "Tchien",
+            "administrativeProvince": "Toffoi Clinic"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_13_GRAND_GEDEH",
+            "district": "MICROPLAN_MO_13_05_TCHIEN",
+            "administrativeProvince": "MICROPLAN_MO_13_05_04_TOFFOI_CLINIC"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Grand Gedeh",
+            "district": "Tchien",
+            "locality": "Toffoi Town",
+            "administrativeProvince": "Toffoi Clinic"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_13_GRAND_GEDEH",
+            "district": "MICROPLAN_MO_13_05_TCHIEN",
+            "locality": "MICROPLAN_MO_13_05_04_01_TOFFOI_TOWN",
+            "administrativeProvince": "MICROPLAN_MO_13_05_04_TOFFOI_CLINIC"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Grand Gedeh",
+            "district": "Tchien",
+            "locality": "Toffoi Town",
+            "village": "Satya Test",
+            "administrativeProvince": "Toffoi Clinic"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_13_GRAND_GEDEH",
+            "district": "MICROPLAN_MO_13_05_TCHIEN",
+            "locality": "MICROPLAN_MO_13_05_04_01_TOFFOI_TOWN",
+            "village": "MICROPLAN_MO_13_05_04_01_12_SATYA_TEST",
+            "administrativeProvince": "MICROPLAN_MO_13_05_04_TOFFOI_CLINIC"
+        }
+    }
+]
+
+kano_state_data = [
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kano state"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_16_KANO_STATE"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kano state",
+            "district": "Kano"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_16_01_KANO",
+            "district": "MICROPLAN_MO_16_01_KANO"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kano state",
+            "district": "Kano",
+            "administrativeProvince": "Ajingi"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_16_KANO_STATE",
+            "district": "MICROPLAN_MO_16_01_KANO",
+            "administrativeProvince": "MICROPLAN_MO_16_01_01_AJINGI"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kano state",
+            "district": "Kano",
+            "locality": "Ajingi l",
+            "administrativeProvince": "Ajingi"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_16_KANO_STATE",
+            "district": "MICROPLAN_MO_16_01_KANO",
+            "locality": "MICROPLAN_MO_16_01_01_01_AJINGI_L",
+            "administrativeProvince": "MICROPLAN_MO_16_01_01_AJINGI"
+        }
+    },
+    {
+        "hierarchy": {
+            "country": "Nigeria",
+            "province": "Kano state",
+            "district": "Kano",
+            "locality": "Ajingi l",
+            "village": "Fulatan hp",
+            "administrativeProvince": "Ajingi"
+        },
+        "codes": {
+            "country": "MICROPLAN_MO",
+            "province": "MICROPLAN_MO_16_KANO_STATE",
+            "district": "MICROPLAN_MO_16_01_KANO",
+            "locality": "MICROPLAN_MO_16_01_01_01_AJINGI_L",
+            "village": "MICROPLAN_MO_16_01_01_01_01_FULATAN_HP",
+            "administrativeProvince": "MICROPLAN_MO_16_01_01_AJINGI"
+        }
+    }
+]
